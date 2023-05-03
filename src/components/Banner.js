09af3bb2e9e639277fb5327bad9bfa8a76207ef5
Eibon <ninetimes64@gmail.com>
2023-05-03 17:30:29 -0500

    created Navbar and logic and updated CSS

---
+++ src/components/Banner.js
@@ -0,0 +1,11 @@
+import { Container, Row} from 'react-bootstrap';
+import { ArrowRightCircle } from 'react-bootstrap-icons';
+
+export const Banner = () => {
+    return (
+        <section className="banner" id="home">
+            <Container>
+                <Row className="align-items-center">
+                    <Col xs={12} md={6} xl={7}>
+                        <span className="tagline">Welcome!</span>
+    )
