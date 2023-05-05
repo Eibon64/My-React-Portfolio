e0dac544b2917fcf96d90e943aa04528db713fc3
Eibon <ninetimes64@gmail.com>
2023-05-05 10:53:11 -0500

    created contact componentn

---
+++ src/components/Contact.js
@@ -0,0 +1,16 @@
+import { useState } from "react";
+
+export const Contact = () => {
+    const formInitialDetails = {
+        firstName: '',
+        lastName: '',
+        email: '',
+        phone: '',
+        message: ''
+    }
+
+    const [formDetails, setFormDetails] = useState(formInitialDetails);
+    const [buttonText, setButtonText] = useState('Send');
+    const [status, setStatus] = useState('');
+
+    
