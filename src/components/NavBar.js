25fde2b2837a116896c55b5eec8201186263d402
Eibon <ninetimes64@gmail.com>
2023-05-05 13:24:46 -0500

    linked github page to footer and navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -45,14 +45,13 @@ export const NavBar = () => {
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="ms-auto">
                             <Nav.Link href="#home" className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('home')}>Home</Nav.Link>
-                            <Nav.Link href="#skills" className={activeLink === 'skills' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('skills')}>Skills</Nav.Link>
                             <Nav.Link href="#projects" className={activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('projects')}>Projects</Nav.Link>
                         </Nav>
                         <span className="navbar-text">
                             <div className="social-icon">
-                                <a href="#"><img src={navIcon1} alt="" /></a>
-                                <a href="#"><img src={navIcon2} alt="" /></a>
-                                <a href="#"><img src={navIcon3} alt="" /></a>
+                                <a href="https://www.linkedin.com/in/eeann-diller-457842258/"><img src={navIcon1} alt="linkedIn" /></a>
+                                <a href="https://github.com/Eibon64"><img src={navIcon2} alt="" /></a>
+                                {/* <a href="#"><img src={navIcon3} alt="" /></a> */}
                             </div>
                             <HashLink to='#connect'>
                                 <button className="vvd"><span>Let’s Connect</span></button>
